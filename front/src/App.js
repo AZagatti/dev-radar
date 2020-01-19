a82dcfc506873714eeabd2d0a330cde1f3b90e06
azagatti <andre.zagatti1@gmail.com>
2020-01-19 14:21:42 -0300

    Axios

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 
+import api from "./services/api";
+
 import "./global.css";
 import "./App.css";
 import "./Sidebar.css";
@@ -30,6 +32,15 @@ function App() {
 
   async function handleAddDev(e) {
     e.preventDefault();
+
+    const response = await api.post("/devs", {
+      github_username: githubUsername,
+      techs,
+      latitude,
+      longitude
+    });
+
+    console.log(response.data);
   }
 
   return (
